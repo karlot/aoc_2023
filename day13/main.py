68a8dd5dd8e5c01df70da92e54815b22bdb265fd
Karlo Tisaj <karlo.tisaj@snt.hr>
2023-12-16 19:06:00 +0100

    remade day13 for part1 and part2

--- day13/main.py
+++ day13/main.py
@@ -1,65 +1,31 @@
 import sys
 
+def check_mirrored(p, part):
+    for r in range(1, len(p)):
+        if part == 1:
+            if p[:r][::-1][:len(p[r:])] == p[r:][:len(p[:r][::-1])]: return r
+        else:
+            if sum(sum(0 if a == b else 1 for a, b in zip(x, y)) for x, y in zip(p[:r][::-1], p[r:])) == 1: return r
+    return 0
+
 def main(filename):
     """
     # Results:
+    Part1: 33735
+    Part2: 
     """
     with open(filename) as f:
         # lines = [l.strip() for l in f.readlines()]
-        patterns = [part.split("\n") for part in f.read().strip().split("\n\n")]
-
-    cum = 0
-    for id, part in enumerate(patterns):
-            rows_len = len(part)
-
-            # Check vertical
-            vsym = False
-            sym_row = None
-            for ir in range(rows_len):
-                if ir == rows_len - 1: break            # When last
-                if part[ir] != part[ir + 1]: continue   # If we dont match with next, skip
-                vsym = True
-                sym_row = ir + 1
-                # print(sym_row, ir + 1, rows_len - ir -1)
-                for offset in range(min(ir + 1, rows_len - ir -1)):
-                    if part[ir - offset] != part[ir + 1 + offset]:
-                        vsym = False
-                        break
-                if vsym:
-                    break
-            if vsym:
-                print(f"{id} Vertical on row: {sym_row}")
-                cum += 100 * sym_row
-                continue
-            
-            # Check horizontal
-            hsym = False
-            sym_col = None
-            tpart = list(zip(*[[c for c in r] for r in part]))
-
-            cols_len = len(tpart)
-            for ic in range(cols_len):
-                if ic == cols_len - 1: break            # When last
-                # print(ic, tpart[ic], tpart[ic + 1], tpart[ic] == tpart[ic + 1])
-                if tpart[ic] != tpart[ic + 1]: continue # If we dont match with next, skip
-                hsym = True
-                sym_col = ic + 1
-                for offset in range(min(ic + 1, cols_len - ic -1)):
-                    if tpart[ic - offset] != tpart[ic + 1 + offset]:
-                        hsym = False
-                        break
-                if hsym:
-                    break
-            if hsym:
-                print(f"{id} Horizontal on col: {sym_col}")
-                cum += sym_col
-                continue
-
-            print(f"{id} is not symetric!")
-        
-
-    print(f"Part1: {cum}")
-    print(f"Part2: {None}")
+        patterns = [part.splitlines() for part in f.read().split("\n\n")]
+    
+    cumulated = [0, 0, 0]
+    for p in patterns:
+        for part in [1, 2]:
+            cumulated[part] += check_mirrored(p, part) * 100
+            cumulated[part] += check_mirrored(list(zip(*p)), part)
+    
+    print(f"Part1: {cumulated[1]}")
+    print(f"Part2: {cumulated[2]}")
             
 
 if __name__ == "__main__":

50e1977cdb0c707ef5d53e20c51df2d684285a3e
Karlo Tisaj <karlo.tisaj@snt.hr>
2023-12-28 15:38:50 +0100

    day21 part2, found some solution... had to get a looot of clues from other solutions

--- day21/main.py
+++ day21/main.py
@@ -74,11 +74,36 @@ def main(filename):
     # ---------------------------------
     # Part 2
     # ---------------------------------
-    # if run_part2:
-    #     target_steps = 26501365
-    #     x = target_steps % (2 * rows)
-    #     print(x)
-    #     print(f"Part2: {None}")
+    # Scrambled some ideas from net, and tried it out... took some time to make it work
+    if run_part2:
+        target_steps = 26501365
+
+        # Get set of all plot coordinates in base matrix both "." and "S" are considered "plots"
+        gps = {(i, j) for i in range(rows) for j in range(rows) if grid[i][j] in '.S'}
+        
+        visited = {start}   # Visited set
+        new     = {start}   # New set
+        cache   = {0:1}     # Cache
+        k       = target_steps // rows  # quotient
+        r       = target_steps % rows   # remainder
+
+        for c in range(1, r + 2 * rows + 1):
+            updated_nps = set()
+            for cp in new:
+                for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    np = (cp[0] + dir[0], cp[1] + dir[1])
+                    if np not in visited and (np[0] % rows, np[1] % rows) in gps:
+                        updated_nps.add(np)
+
+            visited = visited.union(updated_nps)
+            new = updated_nps            
+            cache[c] = len(new) + (cache[c - 2] if c > 1 else 0)
+
+        d2 = cache[r + 2 * rows] + cache[r] - 2 * cache[r + rows]
+        d1 = cache[r + 2 * rows] - cache[r + rows]
+        total_plots = cache[r + 2 * rows] + (k - 2) * (2 * d1 + (k - 1) * d2) // 2
+
+        print(f"Part2: {total_plots}")
             
 
 if __name__ == "__main__":
